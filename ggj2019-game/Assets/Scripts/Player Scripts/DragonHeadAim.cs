ebfd0e878c085f96c425ec1c4c05831483a79798
Sean Harrison <seanharrs97@hotmail.com>
2019-01-26 23:02:35 +1000

    Comment broken code, clarify working code

--- ggj2019-game/Assets/Scripts/Player Scripts/DragonHeadAim.cs
+++ ggj2019-game/Assets/Scripts/Player Scripts/DragonHeadAim.cs
@@ -23,26 +23,28 @@ public class DragonHeadAim : MonoBehaviour
         Debug.Log("Cursor: " + cursor);
         Debug.Log("Head: " + Head.transform.position);
         Debug.Log("Target: " + Target);
-        //Head.transform.LookAt(-Target);
+		//Head.transform.LookAt(-Target); //doesnt work (weird, thought it would act the same as below)
 
-        Head.transform.forward = -(Target - Head.transform.position).normalized;
-        Head.transform.rotation = Quaternion.Euler(Head.transform.rotation.x, 0, Head.transform.rotation.z);
-        //Head.transform.rotation = Quaternion.Euler(Head.transform.rotation.x - 57.84f, Head.transform.rotation.y + 32.48f, Head.transform.rotation.z - 55.6f);
-        //Head.transform.rotation = Head.transform.rotation * Quaternion.Euler(Offset);
-        Debug.DrawLine(Head.transform.position, Target, Color.red);
-        
-        //Make body face
-        if (Target.x < transform.position.x)
-        {
-            //Look left
-            transform.rotation = Quaternion.Euler(Vector3.up * 270);
-        }
-        else
-        {
-            //Look right
-            transform.rotation = Quaternion.Euler(Vector3.up * 90);
-        }
-    }
+		Head.transform.forward = -(Target - Head.transform.position).normalized;
+		//Head.transform.rotation = Quaternion.Euler(Head.transform.rotation.x, 0, Head.transform.rotation.z); //doesnt help, breaks it further
+
+		//Not sure what these two lines are, am guessing we established they dont work
+		//Head.transform.rotation = Quaternion.Euler(Head.transform.rotation.x - 57.84f, Head.transform.rotation.y + 32.48f, Head.transform.rotation.z - 55.6f);
+		//Head.transform.rotation = Head.transform.rotation * Quaternion.Euler(Offset);
+		Debug.DrawLine(Head.transform.position, Target, Color.red);
+
+		//Make body face
+		if(Target.x < transform.position.x)
+		{
+			//Look left
+			transform.rotation = Quaternion.Euler(Vector3.up * 270);
+		}
+		else
+		{
+			//Look right
+			transform.rotation = Quaternion.Euler(Vector3.up * 90);
+		}
+	}
 
     Vector2 GetCursor()
     {
@@ -51,6 +53,7 @@ public class DragonHeadAim : MonoBehaviour
             (
                 new Vector3
                 (
+					//Inverting it didnt help, broke more stuff
                     /*Camera.main.pixelWidth -*/ Input.mousePosition.x,
                     /*Camera.main.pixelHeight -*/ Input.mousePosition.y,
                     Vector3.Distance
