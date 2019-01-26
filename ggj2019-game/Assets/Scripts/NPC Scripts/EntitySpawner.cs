03d7fe415d780839f3f0cd3069186faa1e16cb4d
Unknown <vivekthapar3@gmail.com>
2019-01-26 17:56:13 +1000

    Fire Breath and Entity Spawner
    - added fire breath control
    - added temporary entity spawener

---
+++ ggj2019-game/Assets/Scripts/NPC Scripts/EntitySpawner.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EntitySpawner : MonoBehaviour
+{
+    public GameObject entity;
+    [Tooltip("Maximum amount of entities in game at any given time")]
+    public int maxEntities;
+    [Tooltip("Maximum range from the player entites will spawn")]
+    public float maxRange;
+    public GameObject spawnPoint;
+    
+    private float dist;
+
+    [Space]
+    public GameObject player;
+
+    public int curEntities;
+    public List<GameObject> entities = new List<GameObject>();
+    private bool side;
+
+    void Update()
+    {
+        curEntities = entities.Count;    
+
+        if (curEntities < maxEntities){
+            Spawn();
+        }
+
+        // Debug
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            RemoveEntity(0);
+        }
+    }
+
+    private void Spawn()
+    {
+        dist = spawnPoint.transform.position.x + Random.Range(0, maxRange);
+
+        if (side)
+        {
+            GameObject o = Instantiate(entity, new Vector3(player.transform.position.x + dist, 0, 0), Quaternion.identity);
+            entities.Add(o);
+            side = !side;
+        }
+        else
+        {
+            GameObject o = Instantiate(entity, new Vector3(player.transform.position.x - dist, 0, 0), Quaternion.identity);
+            entities.Add(o);
+            side = !side;
+        }
+    }
+
+    private void RemoveEntity(int entityIndex) 
+    {
+        entities.Remove(entities[entityIndex]);
+        Destroy(entities[entityIndex]);
+    }
+}
