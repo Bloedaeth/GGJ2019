cd12a2b1b6f88788d54c0a4b2e9e3f7716d19aca
Sean Harrison <seanharrs97@hotmail.com>
2019-01-27 12:27:01 +1000

    EnterCave and LeaveCave load their respective scenes

--- ggj2019-game/Assets/Scripts/Scene Scripts/LeaveCave.cs
+++ ggj2019-game/Assets/Scripts/Scene Scripts/LeaveCave.cs
@@ -1,16 +1,12 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
+[RequireComponent(typeof(BoxCollider))]
 public class LeaveCave : MonoBehaviour
 {
-    
-    private void OnTriggerEnter(Collider other)
+	private void OnTriggerEnter(Collider other)
     {
         if (other.transform.tag == "Player")
-        {
-            //TODO change scene to overworld
-            Debug.Log("Loading zone hit");
-        }
+            FindObjectOfType<LevelManager>().LoadLevelAsync("Main", LoadSceneMode.Additive);
     }
 }
